a6ecfc1bd075139d2568f51d2e6c7d9b33389e8d
atomicn50 <tinhle46@gmail.com>
2018-08-18 15:48:50 -0700

    added youtube service

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,44 @@
-angular.module('video-player')
-.service('youTube', function(){
-  // TODO
+angular.module('video-player').service('youTube', function($http) {
+  this.search = function() {
+    $http({
+      method: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      params: {
+        part: 'snippet',
+        videoEmbeddable: true,
+        type: 'video',
+        q: '',
+        maxResults: 5,
+        key: YOUTUBE_API_KEY
+      }
+    }).then(
+      response => {
+        console.log('SUCESS', response);
+      },
+      fail => {
+        console.log('FAILED', fail);
+      }
+    );
+  };
 });
+
+// $http
+//     .get(
+//       'https://www.googleapis.com/youtube/v3/search',
+//       (params: {
+//         part: 'snippet',
+//         videoEmbeddable: true,
+//         type: 'video',
+//         q: 'Learn angular',
+//         maxResults: 5,
+//         key: YOUTUBE_API_KEY
+//       })
+//     )
+//     .then(
+//       response => {
+//         console.log('SUCESS', response);
+//       },
+//       fail => {
+//         console.log('FAILED', fail);
+//       }
+//     );
